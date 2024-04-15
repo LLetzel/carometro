621d854e3f6b583fd725a773f1fc8d734e52348e
LLetzel <lucasdiasletzel@gmail.com>
2024-04-14 22:50:30 -0300

    button classroom

--- carometro/src/pages/home.jsx
+++ carometro/src/pages/home.jsx
@@ -2,16 +2,35 @@ import { useState } from 'react';
 import '../App.css';
 import CarouselCar from './carousel.jsx';
 import Cabecalho from './Header.jsx';
+// import { ThumbUp } from 'react-icons/fa'; // Importando ícone da biblioteca
 
 
-function Home() {
+function Home() { //npm install @fortawesome/fontawesome-free para instalar a biblioteca Fortawesome(para icon)
+  function Linkclassroom(){
+    const classroomUrl = 'https://classroom.google.com/'
+    history.push(classroomUrl); // Redirecionando para o URL
+  };
+
   return (
+
+    
     
     <>
     <Cabecalho></Cabecalho>
+    <div className="containerhomefull">
+        <button onClick={() => window.open(
+              'https://classroom.google.com/u/1/c/NTQyMDY3MzYxOTUx',
+              '_blank'
+            )} id="classroom">
+        <img src="https://w7.pngwing.com/pngs/640/323/png-transparent-computer-icons-google-classroom-others-text-black-icon-download-thumbnail.png" 
+        alt="Icon Classroom" 
+        id='iconclassroom' />
+        CLASSROOM
+        </button>
     <div className="containerhome">
     <CarouselCar></CarouselCar>
     <button id='visualizarturma'>Visualizar turma</button>
+    </div>
     </div>
   </>
   );
